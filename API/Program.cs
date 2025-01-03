6952c44c29aeee8556d326250a192b06307309a6
Hamza <hamzassm18@gmail.com>
2025-01-04 00:26:22 +0300

    Remove obj folder from version control

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,10 @@
+using System.Reflection;
+using Serilog;
+using Serilog.Exceptions;
+using Serilog.Formatting.Json;
+using Serilog.Sinks.Elasticsearch;
+using Serilog.Sinks.File;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +13,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+configureLogging();
+builder.Host.UseSerilog();
 
 var app = builder.Build();
 
@@ -23,3 +33,35 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+void configureLogging()
+{
+    var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+    var configuration = new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{environment}.json", optional: true)
+        .Build();
+
+    Log.Logger = new LoggerConfiguration()
+        .Enrich.FromLogContext()
+        .Enrich.WithExceptionDetails()
+        .WriteTo.Debug()
+        .WriteTo.Console()
+        .WriteTo.Elasticsearch(ConfigureElasticSink(configuration, environment))
+        .Enrich.WithProperty("Environment", environment)
+        .ReadFrom.Configuration(configuration)
+        .CreateLogger();
+}
+
+ElasticsearchSinkOptions ConfigureElasticSink(IConfiguration configuration, string environment)
+{
+    return new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]))
+    {
+        AutoRegisterTemplate = true,
+        IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower()
+            .Replace(".", "-")}-{environment.ToLower()}-{DateTime.UtcNow:yyyy-MM-d}",
+        NumberOfReplicas = 1,
+        NumberOfShards = 2
+    };
+}
